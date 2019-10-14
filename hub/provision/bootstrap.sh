096f34a77d83ac8f7b3034417c1895982feb9790
Jonathan LaBroad <jon.labroad@gmail.com>
2019-10-14 19:52:24 -0400

    bootstrap additions

--- hub/provision/bootstrap.sh
+++ hub/provision/bootstrap.sh
@@ -5,6 +5,12 @@ apt-get update
 apt-get install -y git
 apt-get install -y python3
 apt-get install -y python3-pip
+apt-get install -y libasound2-dev
+
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
 
 # MIDI
 pip3 install python-rtmidi
+pip3 install RPI.GPIO
+pip3 install adafruit-blinka
