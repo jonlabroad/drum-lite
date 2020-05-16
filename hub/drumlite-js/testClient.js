f97df07fb138392dd110d6c00a4915370d2bb8ea
Jonathan LaBroad <jon.labroad@gmail.com>
2020-05-16 08:57:04 -0400

    Update client address

--- hub/drumlite-js/testClient.js
+++ hub/drumlite-js/testClient.js
@@ -1,6 +1,6 @@
 const io = require('socket.io-client');
 const socket = io.connect("http://localhost:3000");
-//const socket = io.connect("http://10.0.0.27:3000");
+//const socket = io.connect("http://10.0.0.23:3000");
 
 socket.on('connect', () => {
     console.log("connected");
